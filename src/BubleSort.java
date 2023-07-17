abcd69d32150d3fa2d7f9238ad277024920618a9
Moiseev Kirill <kilkuruzzza@mail.ru>
2023-07-17 22:25:40 +0300

    add task

---
+++ src/BubleSort.java
@@ -0,0 +1,35 @@
+public class BubleSort {
+    public void sort() {
+        boolean needSort;
+        do {
+            needSort = false;
+            Node node = head;
+            while (node != null && node.next != null){
+                if (node.value > node.next.value){
+                    Node before = node.prev;
+                    Node after = node.next.next;
+                    Node current = node;
+                    Node next = node.next;
+
+                    current.next = after;
+                    current.prev = next;
+                    next.next = current;
+                    next.prev = before;
+                    if (before != null){
+                        before.next = next;
+                    } else {
+                        head = next;
+                    }
+                    if (after != null) {
+                        after.prev = current;
+                    } else  {
+                        tail = current;
+                    }
+
+                    needSort = true;
+                }
+                node = node.next;
+            }
+        } while (needSort);
+    }
+}
